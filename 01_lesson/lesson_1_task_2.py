1ca59f4bfe8a91290d3619a8d56a5226236c6125
kettahmine16 <k.shvetsova/02@gmail.com>
2025-09-24 16:11:22 +0500

    second commit

--- 01_lesson/lesson_1_task_2.py
+++ 01_lesson/lesson_1_task_2.py
@@ -1,3 +1,3 @@
-my_age = ("22")
-my_age = ("25")
+my_age = 22
+my_age = 25
 print(my_age)
--- 01_lesson/lesson_1_task_3.py
+++ 01_lesson/lesson_1_task_3.py
@@ -1,5 +1,5 @@
 print("Как вас зовут?")
-first_name = input('Введите ваше имя')
-last_name = input('Введите вашу фамилию')
+first_name = input("Введите ваше имя: ")
+last_name = input("Введите вашу фамилию: ")
 
-print('Вас зовут' + first_name + last_name)
+print("Вас зовут: " + last_name + " " + first_name)
