9f36ab7f4ce77aef458ae08044fd5603fd00187c
kettahmine16 <k.shvetsova/02@gmail.com>
2025-10-31 12:37:09 +0500

    lesson 6.2

--- 06_lesson/lesson06_task1.py
+++ 06_lesson/lesson06_task1.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome(service=ChromeService(
     ChromeDriverManager().install()))
@@ -10,15 +12,23 @@ driver.get("https://www.google.com/")
 
 driver.maximize_window()
 
-driver.implicitly_wait(20)
 driver.get("http://uitestingplayground.com/ajax")
+wait = WebDriverWait(driver, 20)
 
-blue_btn = "#ajaxButton"
-press_btn = driver.find_element(By.CSS_SELECTOR, blue_btn)
+press_btn = wait.until(EC.element_to_be_clickable(
+    (By.CSS_SELECTOR, "#ajaxButton")))
 press_btn.click()
 
-flash_success = driver.find_element(By.CSS_SELECTOR, "#content")
-txt = flash_success.find_element(By.CSS_SELECTOR, "p.bg-success").text
+expected_text = "Data loaded with AJAX get request."
+wait.until(
+    EC.text_to_be_present_in_element(
+        (By.CSS_SELECTOR, "p.bg-success"),
+        expected_text
+    )
+)
+
+flash_success_element = driver.find_element(By.CSS_SELECTOR, "p.bg-success")
+txt = flash_success_element.text
 
 print(txt)
 
